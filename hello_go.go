b63e24f937548a148ecdf2f86e62181bf7993018
ahoythar@gmail.com <ahoythar@gmail.com>
2019-06-09 21:15:03 -0500

    First commit

---
+++ hello_go.go
@@ -0,0 +1,29 @@
+package main
+
+func main () {
+}
+
+const spanish = "Spanish"
+const french = "French"
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+
+func Hello(name, language string) string {
+   if len(name) == 0 {
+      name = "World"
+   }
+   return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+   switch language {
+   case french:
+      prefix = frenchHelloPrefix
+   case spanish:
+      prefix = spanishHelloPrefix
+   default:
+      prefix = englishHelloPrefix
+   }
+   return
+}
