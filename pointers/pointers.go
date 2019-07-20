fe5f4072c3e1d94de6578e76ec5ebd28bf0e4e92
Mark Johnson <ahoythar@gmail.com>
2019-07-19 22:32:09 -0500

    Added starting files for pointers chapter

---
+++ pointers/pointers.go
@@ -0,0 +1,17 @@
+package pointers
+
+func main() {
+
+}
+
+type Wallet struct {
+	balance int
+}
+
+func (w Wallet) Deposit(amount int) {
+	w.balance += amount
+}
+
+func (w Wallet) Balance() int {
+	return w.balance
+}
