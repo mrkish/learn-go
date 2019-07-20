a2c938723f381c6aa5387c2081146ccc18525e19
Mark Johnson <ahoythar@gmail.com>
2019-07-20 14:38:53 -0500

    Full code and tests for pointers chapter

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -1,17 +1,43 @@
 package pointers
 
+import (
+	"errors"
+	"fmt"
+)
+
 func main() {
 
 }
 
+type Bitcoin int
+
+var ErrInsufficientFunds = errors.New("Attempted to overdraw")
+
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
-func (w Wallet) Deposit(amount int) {
+type Stringer interface {
+	String() string
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
 	w.balance += amount
 }
 
-func (w Wallet) Balance() int {
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return ErrInsufficientFunds
+	}
+	w.balance -= amount
+	return nil
+}
+
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
