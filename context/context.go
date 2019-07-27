c6b05dcd8a41f57a41fdd34767510529665550d7
ahoythar@gmail.com <ahoythar@gmail.com>
2019-07-26 20:57:25 -0500

    Added the starting bits of Context chapter

---
+++ context/context.go
@@ -0,0 +1,17 @@
+package context
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type Store interface {
+	Fetch() string
+	Cancel()
+}
+
+func Server(store Store) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, store.Fetch())
+	}
+}
