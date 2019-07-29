042efd2e19b5841aa80ffe80db800150c89eb068
ahoythar@gmail.com <ahoythar@gmail.com>
2019-07-29 07:55:28 -0500

    Roman numeral brute force switching

--- propertyBasedTests/property.go
+++ propertyBasedTests/property.go
@@ -5,8 +5,24 @@ import "strings"
 func ConvertToRoman(number int) string {
 	var result strings.Builder
 
-	for i := 0; i < number; i++ {
-		result.WriteString("I")
+	for number > 0 {
+		switch {
+		case number > 9:
+			result.WriteString("X")
+			number -= 10
+		case number > 8:
+			result.WriteString("IX")
+			number -= 9
+		case number > 4:
+			result.WriteString("V")
+			number -= 5
+		case number > 3:
+			result.WriteString("IV")
+			number -= 4
+		default:
+			result.WriteString("I")
+			number--
+		}
 	}
 
 	return result.String()
--- propertyBasedTests/property_test.go
+++ propertyBasedTests/property_test.go
@@ -12,6 +12,12 @@ func TestRomanNumerals(t *testing.T) {
 		{"2 gets converted to II", 2, "II"},
 		{"3 gets converted to III", 3, "III"},
 		{"4 gets converted to IV", 4, "IV"},
+		{"5 gets converted to V", 5, "V"},
+		{"6 gets converted to VI", 6, "VI"},
+		{"7 gets converted to VII", 7, "VII"},
+		{"8 gets converted to VIII", 8, "VIII"},
+		{"9 gets converted to IX", 9, "IX"},
+		{"10 gets converted to X", 10, "X"},
 	}
 
 	for _, test := range cases {
