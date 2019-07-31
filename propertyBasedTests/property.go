e9def7b0dcf3b9a0b910a68b3af19e58585e169b
ahoythar@gmail.com <ahoythar@gmail.com>
2019-07-31 08:07:10 -0500

    Added code and tests to convert from a arabic numeral to a Roman numeral

--- propertyBasedTests/property.go
+++ propertyBasedTests/property.go
@@ -2,26 +2,33 @@ package propertyBasedTests
 
 import "strings"
 
+type RomanNumeral struct {
+	Value  int
+	Symbol string
+}
+
+var RomanNumerals = []RomanNumeral{
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{50, "L"},
+	{40, "XL"},
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
 func ConvertToRoman(number int) string {
 	var result strings.Builder
 
-	for number > 0 {
-		switch {
-		case number > 9:
-			result.WriteString("X")
-			number -= 10
-		case number > 8:
-			result.WriteString("IX")
-			number -= 9
-		case number > 4:
-			result.WriteString("V")
-			number -= 5
-		case number > 3:
-			result.WriteString("IV")
-			number -= 4
-		default:
-			result.WriteString("I")
-			number--
+	for _, numeral := range RomanNumerals {
+		for number >= numeral.Value {
+			result.WriteString(numeral.Symbol)
+			number -= numeral.Value
 		}
 	}
 
--- propertyBasedTests/property_test.go
+++ propertyBasedTests/property_test.go
@@ -18,6 +18,19 @@ func TestRomanNumerals(t *testing.T) {
 		{"8 gets converted to VIII", 8, "VIII"},
 		{"9 gets converted to IX", 9, "IX"},
 		{"10 gets converted to X", 10, "X"},
+		{"20 gets converted to XX", 20, "XX"},
+		{"39 gets converted to XXXIX", 39, "XXXIX"},
+		{"40 gets converted to XL", 40, "XL"},
+		{"47 gets converted to XLVII", 47, "XLVII"},
+		{"49 gets converted to XLIX", 49, "XLIX"},
+		{"50 gets converted to L", 50, "L"},
+		{"100 gets converted to C", 100, "C"},
+		{"400 gets converted to CD", 400, "CD"},
+		{"500 gets converted to D", 500, "D"},
+		{"900 gets converted to CM", 900, "CM"},
+		{"1000 gets converted to M", 1000, "M"},
+		{"2019 gets converted to MMXIX", 2019, "MMXIX"},
+		{"4000 gets converted to MMMM", 4000, "MMMM"},
 	}
 
 	for _, test := range cases {
