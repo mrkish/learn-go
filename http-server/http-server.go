bf5e32b702d86ccc22b2034a244468182ef49dad
ahoythar@gmail.com <ahoythar@gmail.com>
2019-09-29 12:45:22 -0500

    Finished http server section, with integration test

--- http-server/http-server.go
+++ http-server/http-server.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	// handler := http.HandlerFunc(PlayerServer)
-	server := &PlayerServer{&InMemoryPlayerStore{}}
+	server := &PlayerServer{NewInMemoryPlayerStore()}
 
 	if err := http.ListenAndServe(":5000", server); err != nil {
 		log.Fatalf("could not listen on port 5000 %v", err)
@@ -20,10 +20,24 @@ func ListenAndServe(addr string, handler Handler) error {
 	return nil
 }
 
-type InMemoryPlayerStore struct{}
+// InMemoryPlayerStore --
+type InMemoryPlayerStore struct {
+	store map[string]int
+}
 
+// NewInMemoryPlayerStore -- gets new player store
+func NewInMemoryPlayerStore() *InMemoryPlayerStore {
+	return &InMemoryPlayerStore{map[string]int{}}
+}
+
+// GetPlayerScore --
 func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
-	return 123
+	return i.store[name]
+}
+
+// RecordWin --
+func (i *InMemoryPlayerStore) RecordWin(name string) {
+	i.store[name]++
 }
 
 // Handler --
@@ -34,6 +48,7 @@ type Handler interface {
 // PlayerStore --
 type PlayerStore interface {
 	GetPlayerScore(name string) int
+	RecordWin(name string)
 }
 
 // PlayerServer --
@@ -42,14 +57,17 @@ type PlayerServer struct {
 }
 
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-
-	if r.Method == http.MethodPost {
-		w.WriteHeader(http.StatusAccepted)
-		return
-	}
-
 	player := r.URL.Path[len("/players/"):]
 
+	switch r.Method {
+	case http.MethodPost:
+		p.processWin(w, player)
+	case http.MethodGet:
+		p.showScore(w, player)
+	}
+}
+
+func (p *PlayerServer) showScore(w http.ResponseWriter, player string) {
 	score := p.store.GetPlayerScore(player)
 
 	if score == 0 {
@@ -59,6 +77,12 @@ func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, score)
 }
 
+func (p *PlayerServer) processWin(w http.ResponseWriter, player string) {
+	p.store.RecordWin(player)
+	w.WriteHeader(http.StatusAccepted)
+}
+
+// PlayerServer -- serves players
 func (p *PlayerServer) PlayerServer(w http.ResponseWriter, r *http.Request) {
 	player := r.URL.Path[len("/players/"):]
 	fmt.Fprint(w, p.store.GetPlayerScore(player))
